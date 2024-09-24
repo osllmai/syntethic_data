0a392fa41274d212bbeb0840e36fa17307b53714
sadra saremi <sadra@nematifamilyfundation.onmicrosoft.com>
2024-09-24 06:09:22 -0700

    Delete IndoxGen/SynthCore/SynthDataGen directory

--- IndoxGen/SynthCore/SynthDataGen/__init__.py
+++
@@ -1,7 +0,0 @@
-from .setup import DataFromAttributedPrompt
-from .setup import DataFromPrompt
-from .setup import DataGenerationPrompt
-from .setup import FewShotPrompt
-from .setup import PromptBase
-from .upload_data import CSV
-from .upload_data import Excel
--- IndoxGen/SynthCore/SynthDataGen/setup/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .data_from_attribute import DataFromAttributedPrompt
-
-from .data_from_prompt import DataFromPrompt
-from .few_shot_prompt import FewShotPrompt
-from .prompt_base import PromptBase
-from .data_generation_prompt import DataGenerationPrompt
