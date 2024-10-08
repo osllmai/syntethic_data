c13ff6f6ff910c89698f2d2ea5d3936c7d1f3978
Fateme Ghasemi <fatemeghasemi491@gmail.com>
2024-10-08 13:07:44 +0330

    Add files via upload

--- indoxGen/synthCore/__init__.py
+++ indoxGen/synthCore/__init__.py
@@ -5,3 +5,5 @@ from .SynthDataGenJudgeLLM.synthetic_data_generation import SyntheticDataGenerat
 from .SynthDataGenJudgeLLM.synthetic_data_generation_humanfeedback import SyntheticDataGeneratorHF
 from .GAN.gan import TabularGANTrainer
 from .GAN.config import TabularGANConfig
+from .GAN.evaluation import train_and_evaluate_classifier, evaluate_utility, evaluate_statistical_similarity, evaluate_privacy
+
