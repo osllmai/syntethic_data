6fafa6d7291a39dae5bc643c1409b27be0ea629b
unknown <ashkan.eskandari.dev@gmail.com>
2024-10-10 13:33:15 +0330

    update

--- libs/indoxGen_tensor/__init__.py
+++ libs/indoxGen_tensor/__init__.py
@@ -1,6 +1,7 @@
 from .GAN.gan import TabularGANTrainer
 from .GAN.config import TabularGANConfig
-from .GAN.evaluation import train_and_evaluate_classifier, evaluate_utility, evaluate_statistical_similarity, evaluate_privacy
+from .GAN.evaluation import train_and_evaluate_classifier, evaluate_utility, evaluate_statistical_similarity, \
+    evaluate_privacy,plot_distributions,evaluate_data_drift
 # import importlib.metadata
 #
 # __version__ = importlib.metadata.version("IndoxGen_tenor")
